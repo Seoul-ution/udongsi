60e0b940f786e81bb2b680ae8e37275b21ad2311
sooooscode <sooooscode@gmail.com>
2025-11-29 03:50:59 +0900

    add(dummy.sql): cart(장바구니) 더미값 추가

---
+++ back/sql/dummy.sql
@@ -0,0 +1,8 @@
+
+/* ============================
+   4. cart_item (user_id = 1 고정)
+   ============================ */
+INSERT INTO cart_item (cart_item_id, user_id, dish_id, quantity) VALUES
+                                                                     (1, 1, 1, 2),
+                                                                     (2, 1, 3, 1),
+                                                                     (3, 1, 5, 1);
